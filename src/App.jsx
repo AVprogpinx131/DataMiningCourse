646e20f8404921c273f1df5cadc40cf1654da9ef
andruv <andruv@taltech.ee>
2025-02-07 21:36:04 +0200

    data visualization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SupplierProductMatrix from './components/SupplierProductMatrix'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <SupplierProductMatrix />
+    </div>
+  )
+}
+
+export default App
